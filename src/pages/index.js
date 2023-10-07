1634c9ef8b3ab13fff7cd4d0426ded1ba57480ea
MIRAGE <mthakore12@gmail.com>
2023-10-07 23:02:17 +0530

    font andpriject pagesetted basic

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,53 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
-const IndexPage = () => {
-  return(
-    <div>
-      Hello
+const IndexPage = () => (
+  <div className=" h-screen w-screen bg-[#101418] font-leagueGothic ">
+    <div className="h-[35%] w-screen bg-[#101418] flex flex-col ">
+      <div className="h-full w-screen bg-[#101418] p-5">
+        <div className="h-full w-full rounded-2xl bg-[#181c20]">
+          <div className="flex h-full w-full flex-col justify-end gap-2 p-6">
+            <span className="text-6xl font-bold text-white ">PROJECT</span>
+            <span className="text-white text-lg">Case studies of my latest work.</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-  
-}
+    <div className="grid grid-cols-2 gap-4 w-screen h-screen  bg-[#101418] p-4 ">
+      {/* Card 1 */}
+      <div className="rounded-xl gradient-background p-4 text-white w-full h-full">
+        {/* Card content goes here */}
+      </div>
 
+      {/* Repeat for cards 2 to 12 */}
+      {/* Card 2 */}
+      <div className="rounded-xl bg-[#181c20] p-4 text-white w-full h-full">
+        {/* Card content goes here */}
+      </div>
+      <div className="rounded-xl bg-[#181c20] p-4 text-white w-full h-full">
+        {/* Card content goes here */}
+      </div>
+      <div className="rounded-xl bg-[#181c20] p-4 text-white w-full h-full">
+        {/* Card content goes here */}
+      </div>
+      <div className="rounded-xl bg-[#181c20] p-4 text-white w-full h-full">
+        {/* Card content goes here */}
+      </div>
+
+      <div className="rounded-xl bg-[#181c20] p-4 text-white w-full h-full">
+        {/* Card content goes here */}
+      </div>
+
+      {/* ... Repeat for cards 3 to 12 ... */}
+    </div>
+  </div>
+)
+
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        leagueGothic: ["League Gothic", "sans"],
+      },
+    },
   },
   plugins: [],
 }
